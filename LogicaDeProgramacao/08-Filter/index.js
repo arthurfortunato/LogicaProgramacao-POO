98bf97fc806e41d58763deda5961e4b8eee808a8
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-10 17:21:57 -0300

    Filter

---
+++ LogicaDeProgramacao/08-Filter/index.js
@@ -0,0 +1,12 @@
+const vetor = [1,2,3,4,5,6,7,8,9,10];
+
+const pares = vetor.filter(x => x % 2 === 0);
+
+console.log(pares);
+
+
+const vetor3 = [10,4,5,6,2,7,3,8,9,1];
+
+const vetor4 = vetor.filter(x => x < 8);
+
+console.log(vetor4);
