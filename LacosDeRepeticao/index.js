08bdf5505efee658dd3bb70d02a1dfd45e8d5dba
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-10 13:36:49 -0300

    Laços de Repetição

---
+++ LacosDeRepeticao/index.js
@@ -0,0 +1,19 @@
+let count = 1;
+
+//WHILE
+while (count <= 10) {
+  console.log(count)
+  count++;
+}
+
+//DO WHILE
+do {
+  console.log(count)
+  count++;
+
+} while (count <= 10);
+
+//FOR
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
