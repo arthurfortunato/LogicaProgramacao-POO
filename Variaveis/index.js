85dfba97d3d603c1a18d32fa65915c943ca33a7c
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-10 09:35:52 -0300

    Variáveis

---
+++ Variaveis/index.js
@@ -0,0 +1,26 @@
+//boolean
+let a = true;
+let b = false;
+
+//Number
+let c = 10;
+let d = 11.5;
+
+//String
+let e = "teste";
+
+//Undefined
+let f;
+let g = undefined;
+
+//function
+let h = function(){};
+let i = () => {};
+
+//object
+let j = {};
+let k = []; //array
+let l = null;
+
+//const
+const pi = 3.1415;
