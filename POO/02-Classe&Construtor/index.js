572999c508f380798cc0a9ccb7781ea969968134
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-11 16:54:27 -0300

    Classe e Construtor

---
+++ POO/02-Classe&Construtor/index.js
@@ -0,0 +1,12 @@
+class Pessoa{
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+}
+
+const pessoa1 = new Pessoa('Arthur', 28);
+console.log(pessoa1)
+
+const pessoa2 = new Pessoa('Juliana', 25);
+console.log(pessoa2)
